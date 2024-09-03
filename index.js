d573b5bb3cc0f3719d8de33596572b8256b0d443
Ali <152992288+Ali-192004@users.noreply.github.com>
2024-09-03 09:44:28 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const myData = {
+    first_name: 'Aldrin',
+    last_name: 'Amantillo',
+    age: 20,
+    school_email: 'alra.amantillo.swu@phinmaed.com',
+};
+
+app.get('/firstname', (req, res) => {
+    res.send(myData.first_name.toUpperCase());
+});
+
+app.get('/lastname', (req, res) => {
+    res.send(myData.last_name.toUpperCase());
+});
+
+app.get('/age', (req, res) => {
+    res.send(myData.age.toString());
+});
+
+app.get('/schoolemail', (req, res) => {
+    res.send(myData.school_email);
+});
+
+app.get('/mydata', (req, res) => {
+    res.send(myData)
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
